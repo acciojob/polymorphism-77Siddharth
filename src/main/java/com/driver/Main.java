a90bcc4a8f6f5838d4ebeb1476f593b97d1049f3
Siddharth Agrawal <07mayhand@gmail.com>
2022-12-11 14:31:38 +0530

    polymorphism fucntion product

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+
+        public int product(int x, int y) {
+            System.out.println(x*y);
+
+            return x*y;
+        }
+        public double product(double x, double y) {
+            System.out.println(x*y);
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            System.out.println(x*y*z);
+            return x*y*z;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(1,2);
+        p.product(2,3,4);
+        p.product(3.5,4.5);
+    }
 
 }
